a3440f7b3fd7a2e83ebeec075769fd4178544a77
jededlah07 <walter.laura03@gmail.com>
2025-10-28 22:05:08 -0500

    Commit

---
+++ ArchivosPHP/Variables.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 5;
+$y = "John";
+
+echo $x;
+echo "<br>";
+echo $y;
+?>
+
+</body>
+</html>
+
+
+// 2nd File: ArchivosPHP/Variables.php
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt = "W3Schools.com";
+echo "I love $txt!";
+?>
+
+</body>
+</html>
+
+
+// 3rd File: ArchivosPHP/Variables.php
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt = "W3Schools.com";
+echo "I love " . $txt . "!";
+?>
+
+</body>
+</html>
+
+
+
+// 4th File: ArchivosPHP/Variables.php
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 5;
+$y = 4;
+echo $x + $y;
+?>
+
+</body>
+</html>
+
+
+
+// 5th File: ArchivosPHP/Variables.php
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 5;      // $x is an integer
+$y = "John"; // $y is a string
+
+echo $x;
+echo $y;
+?>
+
+</body>
+</html>
+
+
+
+
+// 6th File: ArchivosPHP/Variables.php
